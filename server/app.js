dd7bfeb127c67314b65a0f8d7097f8a7f68471da
Sergio Paredes <sdj.paredes21@info.uas.edu.mx>
2026-02-06 19:10:53 -0700

    fix: Enable trust proxy for Render rate limiting

--- server/app.js
+++ server/app.js
@@ -12,6 +12,9 @@ const notificationRoutes = require('./routes/notificationRoutes');
 
 const app = express();
 
+// Required for Render (or any proxy) to correctly identify client IP
+app.set('trust proxy', 1);
+
 // Security Headers
 app.use(helmet());
 
