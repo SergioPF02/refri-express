e28c462f341621b866027724963d463f63055a11
Sergio Paredes <sdj.paredes21@info.uas.edu.mx>
2026-02-06 19:19:27 -0700

    Envio de emails

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ const notificationRoutes = require('./routes/notificationRoutes');
 const app = express();
 
 // Required for Render (or any proxy) to correctly identify client IP
+// Triggering new deployment
 app.set('trust proxy', 1);
 
 // Security Headers
